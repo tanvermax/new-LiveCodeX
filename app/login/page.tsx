19f7d1216ccf4ade465a9de75b795320d9dff101
Arifuliit <ariful.iit@gmail.com>
2025-03-17 14:56:28 +0600

    edit page section

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,7 +11,7 @@ const LoginPage = () => {
                     <div className="text-center lg:text-left">
                         <h1 className="text-5xl font-bold">Login now!</h1>
                         <p className="py-6">
-                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                            Provident Cupiditate Voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                             quasi. In deleniti eaque aut repudiandae et a id nisi.
                         </p>
                     </div>
